75984f84ce0019c0fc181557c30d2aa13c39fc28
HandreMelo <34012482+HandreMelo@users.noreply.github.com>
2018-11-19 20:57:59 -0200

    Add files via upload

---
+++ ControladorESP/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace LEDCONTROL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.panel1.BackColor = System.Drawing.SystemColors.Window;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Cursor = System.Windows.Forms.Cursors.NoMove2D;
+            this.panel1.Location = new System.Drawing.Point(50, 50);
+            this.panel1.Margin = new System.Windows.Forms.Padding(5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(255, 255);
+            this.panel1.TabIndex = 1;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown_1);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(50, 7);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(255, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(50, 28);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(256, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 311);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Controlador";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        public System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ ControladorESP/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebSocketSharp;
+using System.Windows.Input;
+
+namespace LEDCONTROL
+{
+    
+    public partial class Form1 : Form
+    {
+        bool painel = false;
+        bool flag = true;
+        WebSocket ws = new WebSocket(@"ws://192.168.4.1:81/");
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+           // ws.OnMessage += (sender, e) =>
+               // Console.WriteLine("Laputa says: " + e.Data);
+
+            ws.Connect();
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            ws.Send("LEDOn");
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+            
+              
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void panel1_DragEnter(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void panel1_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            Point local = new Point(e.X, e.Y);
+            String msg = "";
+            String R = "";
+            String L = "";
+                       
+            if (painel == true)
+            {
+                
+                local.Offset(e.X - 255, e.Y - 255);
+                
+                int X = local.X;
+                int Y = (local.Y)*(-1);
+                msg = "X" + X + "<>" + "Y" + Y;
+                textBox2.Text = msg;
+
+                moverChecker(X, Y);
+
+                int sinal = 1;
+                                
+                if (Y < 0) sinal = -1;
+                if (Y >= 0) sinal = 1;
+
+                if (X < 0) {
+                    R = "R" + ((Y * sinal + X * (-1)) * sinal);
+                    L = "L" + Y;
+                }
+
+                if (X >= 0)
+                {
+                    L = "L" + ((Y * sinal + X) * sinal);
+                    R = "R" + Y;
+                }
+                    ws.Send(R);
+                    ws.Send(L);
+                    textBox1.Text = L + "<>" + R;
+            }
+                      
+        }
+
+        private void panel1_MouseDown_1(object sender, MouseEventArgs e)
+        {
+            painel = !painel;
+            ws.Send("R0");
+            ws.Send("L0");
+            textBox1.Text = "L0 <> R0";
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            ws.Send("{X=250,Y=-250}");
+        }
+
+        public void moverChecker(int x, int y)
+        {
+
+            
+
+        }
+    }
+}
